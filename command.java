51fc894317a50a041714ab6c0e172eb227c41d0d
yogeswari2105 <65902933+yogeswari2105@users.noreply.github.com>
2020-05-30 18:54:15 +0530

    Create command.java

---
+++ command.java
@@ -0,0 +1,10 @@
+public class CommandLine
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hi");
+        System.out.print(args[0]+",");
+        System.out.print(args[1]+"");
+        System.out.println("Welcome John");
+    }
+}
