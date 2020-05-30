28d27df8ffe2123c85f274be2afbbae84b7bc99a
yogeswari2105 <65902933+yogeswari2105@users.noreply.github.com>
2020-05-30 18:56:55 +0530

    Create colorcodetoname.java

---
+++ colorcodetoname.java
@@ -0,0 +1,30 @@
+Class codetoname {
+public static void main(String args[])
+{
+Scanner s =new scanner(system.in);
+System.out.println("enter the color code:");
+char code=s.nextint();
+Switch(code)
+{
+case R:
+ System.out.println("Red");
+ break;
+case B:
+ System.out.println("Blue");
+ break;
+case G:
+ System.out.println("Green");
+ break;
+case O:
+ System.out.println("Orange");
+ break;
+case Y:
+ System.out.println("Yellow");
+ break;
+case W:
+ System.out.println("white");
+ break;
+ }
+ }
+ }
+ 
