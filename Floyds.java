7f81e71aca57470fe8142bb05094b50f61974bd4
yogeswari2105 <65902933+yogeswari2105@users.noreply.github.com>
2020-06-10 19:58:39 +0530

    Create Floyds.java

---
+++ Floyds.java
@@ -0,0 +1,18 @@
+Class Triangle{
+public static void main(String args[]){
+ int i,j,num=1,row;
+ 
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Please enter the number of rows:");
+ row= sc nextInt();
+ 
+ for(i=1;i<=row;i++){
+   for(j=1;i<=i;j++){
+     System.out.println(" * ");
+     
+     }
+   }
+   System.out.println(" ");
+ }
+
+}
